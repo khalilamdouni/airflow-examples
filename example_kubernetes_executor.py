f5e055db33cc747d93b7cdb8c73863868035af8d
Khalil AMDOUNI <amdounikhalil@gmail.com>
2021-10-26 16:11:42 +0200

    using my docker image

--- example_kubernetes_executor.py
+++ example_kubernetes_executor.py
@@ -32,72 +32,17 @@ with DAG(
     catchup=False,
     tags=['example', 'example2'],
 ) as dag:
-    # You don't have to use any special KubernetesExecutor configuration if you don't want to
     @task
     def start_task():
         print_stuff()
 
-    # But you can if you want to
-    kube_exec_config_special = {"KubernetesExecutor": {"image": "airflow/ci:latest"}}
+    kube_exec_config_special = {"KubernetesExecutor": {"image": "khalilamdouni/snowflake-airflow"}}
 
     @task(executor_config=kube_exec_config_special)
     def one_task():
         print_stuff()
 
-    # Use the zip binary, which is only found in this special docker image
-    kube_exec_config_zip_binary = {"KubernetesExecutor": {"image": "airflow/ci_zip:latest"}}
-
-    @task(task_id="two_task", executor_config=kube_exec_config_zip_binary)
-    def assert_zip_binary():
-        """
-        Checks whether Zip is installed.
-
-        :raises SystemError: if zip is not installed
-        """
-        return_code = os.system("zip")
-        if return_code != 0:
-            raise SystemError("The zip binary is not found")
-
-    # Limit resources on this operator/task with node affinity & tolerations
-    affinity = {
-        'podAntiAffinity': {
-            'requiredDuringSchedulingIgnoredDuringExecution': [
-                {
-                    'topologyKey': 'kubernetes.io/hostname',
-                    'labelSelector': {
-                        'matchExpressions': [{'key': 'app', 'operator': 'In', 'values': ['airflow']}]
-                    },
-                }
-            ]
-        }
-    }
-
-    tolerations = [{'key': 'dedicated', 'operator': 'Equal', 'value': 'airflow'}]
-
-    kube_exec_config_resource_limits = {
-        "KubernetesExecutor": {
-            "request_memory": "128Mi",
-            "limit_memory": "128Mi",
-            "tolerations": tolerations,
-            "affinity": affinity,
-        }
-    }
-
-    @task(executor_config=kube_exec_config_resource_limits)
-    def three_task():
-        print_stuff()
-
-    # Add arbitrary labels to worker pods
-    kube_exec_config_pod_labels = {"KubernetesExecutor": {"labels": {"foo": "bar"}}}
-
-    @task(executor_config=kube_exec_config_pod_labels)
-    def four_task():
-        print_stuff()
-
     start_task = start_task()
     one_task = one_task()
-    two_task = assert_zip_binary()
-    three_task = three_task()
-    four_task = four_task()
 
-    start_task >> [one_task, two_task, three_task, four_task]
+    start_task >> one_task
